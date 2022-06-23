218a4faf7fb77cb326275966a53687e33dfe64b8
Robbin Romijnders <r.j.romijnders@gmail.com>
2022-06-23 16:02:38 +0200

    Uploaded to CAU HPC

--- code/test.py
+++ code/test.py
@@ -8,7 +8,8 @@ from tcn import TCN
 
 from utils.data_utils import SAMPLING_FREQUENCY, load_data
 
-ROOT_DIR = "/mnt/neurogeriatrics_data/MobiliseD_TVS/rawdata" if sys.platform == "linux" else "Z:\\MobiliseD_TVS\\rawdata"
+# ROOT_DIR = "/mnt/neurogeriatrics_data/MobiliseD_TVS/rawdata" if sys.platform == "linux" else "Z:\\MobiliseD_TVS\\rawdata"
+ROOT_DIR = "/gxfs_work1/cau/sukne964/Mobilise-D"
 WIN_LEN = int(10 * 60 * SAMPLING_FREQUENCY)
 
 def main():
@@ -17,7 +18,6 @@ def main():
     start_time = time.time()
     (x_train, y_train), (x_test, y_test) = load_data(path=ROOT_DIR, win_len=WIN_LEN)
     end_time = time.time()
-    print(f'Total time: {end_time - start_time:.2f} sec')
 
     # Create model
     inputs = tf.keras.Input(shape=(None, 12), name="inputs")
@@ -26,6 +26,9 @@ def main():
     model = tf.keras.Model(inputs=inputs, outputs=outputs, name="tcn_model")
     model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), metrics=["binary_accuracy"])
     model.summary()
+
+    # Fit model
+    history = model.fit(x=x_train, y=y_train, batch_size=16, epochs=3, validation_data=(x_test, y_test), shuffle=True)
         
     return
     
