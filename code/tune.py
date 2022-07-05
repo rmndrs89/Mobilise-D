5fa31e46e5e9ba20aa7502899dee406386cd03bb
Robbin Romijnders <r.j.romijnders@gmail.com>
2022-07-05 13:34:51 +0200

    Prepared tune.py for running on cluster

--- code/tune.py
+++ code/tune.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
-import os, sys
-from utils.data_utils import SAMPLING_FREQUENCY, load_data
+import os
+from train import BATCH_SIZE
+from utils.data_utils import SAMPLING_FREQUENCY, get_data_generator, split_train_test
 from custom.models import get_multi_output_model
 
 # Define root directory
@@ -9,89 +10,88 @@ from custom.models import get_multi_output_model
 ROOT_DIR = "/gxfs_work1/cau/sukne964/Mobilise-D"
 
 # Define window length
-WIN_LEN = int(6 * SAMPLING_FREQUENCY)
+WIN_LEN = int(10 * SAMPLING_FREQUENCY)
+
+# Define number of input channels
+NUM_INPUT_CHANNELS = 12
 
 # Set max number of epochs
 MAX_EPOCHS = 50
 
-# Define search space of hyperparameters
-HYPERPARAMS = {
-    "NB_FILTERS": [16, 32,64, 128],
-    "KERNEL_SIZE": [3, 5, 7],
-    "MAX_DILATION": [d for d in range(2, 8)]
-}
-
-# Define logs filepath
-LOGS_FILEPATH = "/gxfs_home/cau/sukne964/Mobilise-D/code/training/tuning/runs"
-
-# Define boundaries for the receptive field
-R_min, R_max = int(SAMPLING_FREQUENCY), int(6*SAMPLING_FREQUENCY)
+# Define model checkpoint pathlogs filepath
+CHECKPOINT_FILEPATH = "/gxfs_home/cau/sukne964/Mobilise-D/code/train/tune/01"
 
 # Define callbacks
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
     monitor = "val_loss",
-    factor = 0.1,
+    factor = 0.5,
     patience = 5,
     verbose = 0,
     min_lr = 1e-5
 )
 
-def main():
-    # Load data
-    train_data, val_data, test_data = load_data(ROOT_DIR, win_len=WIN_LEN)
-    
-    # Split features and labels
-    X_train, y1_train, y2_train = train_data
-    X_val, y1_val, y2_val = val_data
-    y2_train = tf.keras.utils.to_categorical(y2_train, num_classes=5)
-    y2_val = tf.keras.utils.to_categorical(y2_val, num_classes=5)
-    
-    # Print dimensions
-    print(f"Shape of X train: {X_train.shape}")
-    print(f"Shape of y1 train: {y1_train.shape}")
-    print(f"Shape of y2 train: {y2_train.shape}")
-    
-    # Iterate over the search space
-    nb_models = 0  # number of models that have been evaluated
-    for kernel_size in HYPERPARAMS["KERNEL_SIZE"]:
-        for max_dilation in HYPERPARAMS["MAX_DILATION"]:
-            for nb_filters in HYPERPARAMS["NB_FILTERS"]:
-                
-                # Compute the receptive field
-                R = 1 + 2 * (kernel_size - 1) * 1 * sum([2**d for d in range(max_dilation)])
-                
-                if (R > R_min) and (R < R_max):
-                    
-                    # Instantiate a model
-                    model = get_multi_output_model(
-                        nb_channels=X_train.shape[-1],
-                        nb_classes=y2_train.shape[-1],
-                        **{'nb_filters': nb_filters,
-                           'kernel_size': kernel_size,
-                           'dilations': [2**d for d  in range(max_dilation)]}
-                    )
-                    
-                    # Train the model for some epochs
-                    model.fit(
-                        x = X_train,
-                        y = {'gait_sequences': y1_train,
-                             'gait_events': y2_train},
-                        batch_size = 16,
-                        epochs = MAX_EPOCHS,
-                        validation_data = (X_val, {'gait_sequences': y1_val,
-                                                   'gait_events': y2_val}),
-                        shuffle = True,
-                        verbose = 0,
-                        callbacks = [reduce_lr, 
-                            tf.keras.callbacks.CSVLogger(
-                                os.path.join(LOGS_FILEPATH, f"{nb_models:02d}.csv"), separator=",", append=False
-                            )
-                        ]
-                    )
+csv_logger = tf.keras.callbacks.CSVLogger(
+    filename = os.path.join(CHECKPOINT_FILEPATH, "trainig.log"),
+    separator = ",",
+    append = False
+)
 
-                    nb_models += 1
-                    if nb_models > 1:
-                        return                   
+model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+    CHECKPOINT_FILEPATH,
+    monitor = "val_loss",
+    verbose = 0,
+    save_best_only = True,
+    save_weights_only = False
+)
+
+def main():
+    
+    # Get list of filenames for train, val and test set
+    train_filenames, val_filenames, test_filenames = split_train_test(path=ROOT_DIR, test_size=0.2)
+    
+    # Get tf.data.Dataset for train and val set
+    train_ds = tf.data.Dataset.from_generator(
+        get_data_generator(list_files=train_filenames,
+                           win_len=WIN_LEN),
+        output_signature=(
+            tf.TensorSpec(shape=(WIN_LEN, NUM_INPUT_CHANNELS), dtype=tf.float32),
+            {"gait_sequences": tf.TensorSpec(shape=(WIN_LEN, 1), dtype=tf.float32),
+             "gait_events": tf.TensorSpec(shape=(WIN_LEN, 5), dtype=tf.float32)}
+        )
+    )
+    val_ds = tf.data.Dataset.from_generator(
+        get_data_generator(list_files=val_filenames,
+                           win_len=WIN_LEN),
+        output_signature=(
+            tf.TensorSpec(shape=(WIN_LEN, NUM_INPUT_CHANNELS), dtype=tf.float32),
+            {"gait_sequences": tf.TensorSpec(shape=(WIN_LEN, 1), dtype=tf.float32),
+             "gait_events": tf.TensorSpec(shape=(WIN_LEN, 5), dtype=tf.float32)}
+        )
+    )
+    
+    # Get batched dataset, infinitely repeating for the training set
+    train_ds = train_ds.shuffle(buffer_size=128).batch(batch_size=BATCH_SIZE).repeat()
+    val_ds = val_ds.batch(batch_size=BATCH_SIZE)
+    
+    # Get a multi-output model, simulatenous predicting
+    # continuous walking periods and discrete gait events
+    model = get_multi_output_model(
+        num_input_channels=NUM_INPUT_CHANNELS,
+        **{"nb_filters": 16,
+           "dilations": [1, 2, 4, 8, 16, 32, 64],
+           "kernel_size": 5}
+    )
+       
+    # Train model on data
+    history = model.fit(
+        train_ds,
+        epochs = 10,
+        steps_per_epoch = 1000,
+        validation_data = val_ds,
+        callbacks = [reduce_lr,
+                     csv_logger,
+                     model_checkpoint]
+    )
     return
 
 if __name__ == "__main__":
